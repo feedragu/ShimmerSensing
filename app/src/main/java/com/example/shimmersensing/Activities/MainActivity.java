aea547a623706eeee1182cd3ac7c161835e2ed6e
Federico Ragusa <ragusafederico97@gmail.com>
2019-10-22 00:21:49 +0200

    Intro shimmer connection

--- app/src/main/java/com/example/shimmersensing/Activities/MainActivity.java
+++ app/src/main/java/com/example/shimmersensing/Activities/MainActivity.java
@@ -2,21 +2,32 @@ package com.example.shimmersensing.Activities;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.util.Pair;
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator;
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.transition.ChangeImageTransform;
+import android.transition.CircularPropagation;
 import android.transition.Explode;
 import android.transition.Fade;
 import android.transition.Slide;
+import android.transition.Transition;
+import android.transition.TransitionSet;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 
 import com.example.shimmersensing.R;
 import com.example.shimmersensing.Utilities.RecyclerAdapter;
@@ -35,6 +46,10 @@ public class MainActivity extends AppCompatActivity implements RecyclerAdapter.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar mToolbar = findViewById(R.id.toolbar_shimmer);
+        //ColorDrawable colorDrawable = new ColorDrawable( Color.TRANSPARENT );
+        //getWindow().setBackgroundDrawable( colorDrawable );
+
 
         shimmerList = findViewById(R.id.shimmer_sensors);
         manager = new GridLayoutManager(MainActivity.this, 2);
@@ -42,24 +57,26 @@ public class MainActivity extends AppCompatActivity implements RecyclerAdapter.O
 
         List<row> list= new ArrayList<>();
         list.add(new row(R.drawable.shimmer_sensor));
+        list.add(new row(R.drawable.shimmer_sensor));
+        list.add(new row(R.drawable.shimmer_sensor));
 
         rAdapter = new RecyclerAdapter(this, list, this);
         shimmerList.setAdapter(rAdapter);
-        //supportPostponeEnterTransition();
-//        Fade fade = new Fade();
-//        fade.setStartDelay(0);
-//        fade.setMode(Fade.MODE_IN);
-//        fade.setDuration(0);
+
         Slide slide =new Slide();
         slide.setSlideEdge(Gravity.TOP);
-        //slide.setDuration(350);
+        slide.setDuration(300);
+        slide.setPropagation(new CircularPropagation());
 
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().setSharedElementExitTransition(new ChangeImageTransform());
-        getWindow().setExitTransition(null);
-        getWindow().setSharedElementEnterTransition(new ChangeImageTransform());
-        getWindow().setEnterTransition(null);
+        Fade fade =new Fade();
+        fade.setDuration(100);
+
+        slide.setInterpolator(new LinearInterpolator());
+        Transition transition = new TransitionSet()
+                .addTransition(fade)
+                .addTransition(slide);
+
+        getWindow().setExitTransition(transition);
     }
 
     @Override
@@ -68,8 +85,9 @@ public class MainActivity extends AppCompatActivity implements RecyclerAdapter.O
 // Pass data object in the bundle and populate details activity.
         Pair<View, String> p1 = Pair.create(findViewById(R.id.ivImage), "shimmer_sensor");
         Pair<View, String> p2 = Pair.create(findViewById(R.id.tvTitle), "shimmer_text");
+        Pair<View, String> p3 = Pair.create(findViewById(R.id.toolbar_shimmer), "toolbar_shimmer");
         ActivityOptionsCompat options = ActivityOptionsCompat.
-                makeSceneTransitionAnimation(this, p1, p2);
+                makeSceneTransitionAnimation(this, p3, p2, p1);
         startActivity(intent, options.toBundle());
 
 
--- app/src/main/java/com/example/shimmersensing/Activities/ShimmerSpec.java
+++ app/src/main/java/com/example/shimmersensing/Activities/ShimmerSpec.java
@@ -7,18 +7,43 @@ import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.transition.ChangeImageTransform;
+import android.os.Handler;
+import android.os.Message;
+import android.transition.CircularPropagation;
 import android.transition.Explode;
+import android.transition.Fade;
 import android.transition.Slide;
 import android.transition.Transition;
 import android.transition.TransitionSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.Toast;
+
+import com.shimmerresearch.android.Shimmer;
+import com.shimmerresearch.android.guiUtilities.ShimmerBluetoothDialog;
+import com.shimmerresearch.android.guiUtilities.ShimmerDialogConfigurations;
+import com.shimmerresearch.android.manager.ShimmerBluetoothManagerAndroid;
+import com.shimmerresearch.bluetooth.ShimmerBluetooth;
+import com.shimmerresearch.driver.CallbackObject;
+import com.shimmerresearch.driver.Configuration;
+import com.shimmerresearch.driver.FormatCluster;
+import com.shimmerresearch.driver.ObjectCluster;
+import com.shimmerresearch.driver.ShimmerDevice;
 
 import com.example.shimmersensing.R;
 
+import java.util.Collection;
+
 public class ShimmerSpec extends AppCompatActivity {
+
+    ShimmerBluetoothManagerAndroid btManager;
+    ShimmerDevice shimmerDevice;
+    String shimmerBtAdd = "00:00:00:00:00:00";  //Put the address of the Shimmer device you want to connect here
+
+    final static String LOG_TAG = "BluetoothManagerExample";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,44 +52,52 @@ public class ShimmerSpec extends AppCompatActivity {
 
         Toolbar mToolbar = findViewById(R.id.toolbar_shimmer);
         mToolbar.setBackgroundColor(getResources().getColor(android.R.color.transparent));
-        ColorDrawable colorDrawable = new ColorDrawable( Color.TRANSPARENT );
+        ColorDrawable colorDrawable = new ColorDrawable( Color.WHITE );
         getWindow().setBackgroundDrawable( colorDrawable );
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-//        Fade fade = new Fade();
-//        fade.setStartDelay(0);
-//        fade.setMode(Fade.MODE_OUT);
-//        fade.setDuration(0);
-
-//        Fade fade =new Fade();
-//        //fade.setDuration(115);
-//        Slide slide =new Slide();
-//        slide.setSlideEdge(Gravity.BOTTOM);
         View layout=findViewById(R.id.view_layout);
-        final Rect epicenter=new Rect(layout.getLeft(), layout.getTop(), layout.getRight(), layout.getBottom());;
+        final Rect epicenter=new Rect(layout.getLeft(), layout.getTop(), layout.getRight(), layout.getBottom());
         Explode explode = new Explode() {
             {
                 super.setEpicenterCallback(new Transition.EpicenterCallback() {
                     @Override
                     public Rect onGetEpicenter(Transition transition) {
+                        Log.i("prova", "funziona");
                         return epicenter;
                     };
 
             });
             }
         };
+        Slide slide =new Slide();
+        slide.setSlideEdge(Gravity.BOTTOM);
+        slide.setDuration(250);
+        CircularPropagation c=new CircularPropagation();
+        c.setPropagationSpeed(3f);
+        slide.setPropagation(c);
+
+        slide.setInterpolator(new LinearInterpolator());
+        Fade fade =new Fade();
+        fade.setDuration(200);
+        fade.setMode(Fade.MODE_IN);
         Transition transition = new TransitionSet()
-                .addTransition(explode);
-        getWindow().setSharedElementExitTransition(new ChangeImageTransform());
+                .addTransition(slide)
+                .addTransition(fade);
+
         getWindow().setEnterTransition(transition);
 
-        getWindow().setSharedElementExitTransition(new ChangeImageTransform());
         Transition transition2 = new TransitionSet()
         .addTransition(explode);
         getWindow().setExitTransition(transition2);
 
+        try {
+            btManager = new ShimmerBluetoothManagerAndroid(this, mHandler);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Couldn't create ShimmerBluetoothManagerAndroid. Error: " + e);
+        }
     }
 
     @Override
@@ -82,5 +115,85 @@ public class ShimmerSpec extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    Handler mHandler;
+
+    {
+        mHandler = new Handler() {
+
+            @Override
+            public void handleMessage(Message msg) {
+
+                switch (msg.what) {
+                    case ShimmerBluetooth.MSG_IDENTIFIER_DATA_PACKET:
+                        if ((msg.obj instanceof ObjectCluster)) {
+
+                            ObjectCluster objectCluster = (ObjectCluster) msg.obj;
+
+                            //Retrieve all possible formats for the current sensor device:
+                            Collection<FormatCluster> allFormats = objectCluster.getCollectionOfFormatClusters(Configuration.Shimmer3.ObjectClusterSensorName.TIMESTAMP);
+                            FormatCluster timeStampCluster = ((FormatCluster) ObjectCluster.returnFormatCluster(allFormats, "CAL"));
+                            double timeStampData = timeStampCluster.mData;
+                            Log.i(LOG_TAG, "Time Stamp: " + timeStampData);
+                            allFormats = objectCluster.getCollectionOfFormatClusters(Configuration.Shimmer3.ObjectClusterSensorName.ACCEL_LN_X);
+                            FormatCluster accelXCluster = ((FormatCluster) ObjectCluster.returnFormatCluster(allFormats, "CAL"));
+                            if (accelXCluster != null) {
+                                double accelXData = accelXCluster.mData;
+                                Log.i(LOG_TAG, "Accel LN X: " + accelXData);
+                            }
+                        }
+                        break;
+                    case Shimmer.MESSAGE_TOAST:
+                        /** Toast messages sent from {@link Shimmer} are received here. E.g. device xxxx now streaming.
+                         *  Note that display of these Toast messages is done automatically in the Handler in {@link com.shimmerresearch.android.shimmerService.ShimmerService} */
+                        Toast.makeText(getApplicationContext(), msg.getData().getString(Shimmer.TOAST), Toast.LENGTH_SHORT).show();
+                        break;
+                    case ShimmerBluetooth.MSG_IDENTIFIER_STATE_CHANGE:
+                        ShimmerBluetooth.BT_STATE state = null;
+                        String macAddress = "";
+
+                        if (msg.obj instanceof ObjectCluster) {
+                            state = ((ObjectCluster) msg.obj).mState;
+                            macAddress = ((ObjectCluster) msg.obj).getMacAddress();
+                        } else if (msg.obj instanceof CallbackObject) {
+                            state = ((CallbackObject) msg.obj).mState;
+                            macAddress = ((CallbackObject) msg.obj).mBluetoothAddress;
+                        }
+
+                        Log.d(LOG_TAG, "Shimmer state changed! Shimmer = " + macAddress + ", new state = " + state);
+
+                        switch (state) {
+                            case CONNECTED:
+                                Log.i(LOG_TAG, "Shimmer [" + macAddress + "] is now CONNECTED");
+                                shimmerDevice = btManager.getShimmerDeviceBtConnectedFromMac(shimmerBtAdd);
+                                if (shimmerDevice != null) {
+                                    Log.i(LOG_TAG, "Got the ShimmerDevice!");
+                                } else {
+                                    Log.i(LOG_TAG, "ShimmerDevice returned is NULL!");
+                                }
+                                break;
+                            case CONNECTING:
+                                Log.i(LOG_TAG, "Shimmer [" + macAddress + "] is CONNECTING");
+                                break;
+                            case STREAMING:
+                                Log.i(LOG_TAG, "Shimmer [" + macAddress + "] is now STREAMING");
+                                break;
+                            case STREAMING_AND_SDLOGGING:
+                                Log.i(LOG_TAG, "Shimmer [" + macAddress + "] is now STREAMING AND LOGGING");
+                                break;
+                            case SDLOGGING:
+                                Log.i(LOG_TAG, "Shimmer [" + macAddress + "] is now SDLOGGING");
+                                break;
+                            case DISCONNECTED:
+                                Log.i(LOG_TAG, "Shimmer [" + macAddress + "] has been DISCONNECTED");
+                                break;
+                        }
+                        break;
+                }
+
+                super.handleMessage(msg);
+            }
+        };
     }
 }
