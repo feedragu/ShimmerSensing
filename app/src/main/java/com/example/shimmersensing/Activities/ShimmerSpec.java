eda137fcdcf686e2d20ca987eb3d12ef072d9a84
ragus <ragusafederico97@gmail.com>
2019-10-29 19:08:44 +0100

    obtained total acceleration

--- app/src/main/java/com/example/shimmersensing/Activities/ShimmerSpec.java
+++ app/src/main/java/com/example/shimmersensing/Activities/ShimmerSpec.java
@@ -2,8 +2,10 @@ package com.example.shimmersensing.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.interpolator.view.animation.LinearOutSlowInInterpolator;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -11,10 +13,12 @@ import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.MediaStore;
 import android.transition.CircularPropagation;
 import android.transition.Explode;
 import android.transition.Fade;
@@ -72,8 +76,8 @@ public class ShimmerSpec extends AppCompatActivity {
     double[] mLPFc = {5};
 
 
-    private static Integer INVALID_OUTPUT=-1;
-    private static int mNumberOfBeatsToAverage=5;
+    private static Integer INVALID_OUTPUT = -1;
+    private static int mNumberOfBeatsToAverage = 5;
     private static Shimmer mShimmerHeartRate;
     private static PPGtoHRAlgorithm mHeartRateCalculation;
     private static ECGtoHRAdaptive mHeartRateCalculationECG;
@@ -88,17 +92,20 @@ public class ShimmerSpec extends AppCompatActivity {
     static private double[] mHPFcECG = {0.5};
     private static int mCount = 0;
     private static int mCountPPGInitial = 0; //skip first 100 samples
-    private static int mRefreshLimit =  10;
+    private static int mRefreshLimit = 10;
     private static double mCurrentHR = -1;
     private static String mSensortoHR = "";
 
 
     final static String LOG_TAG = "ShimmerProject";
     private double sampleRate;
-    private boolean not_config_yet=true;
+    private boolean not_config_yet = true;
     private GraphView graph;
     private LineGraphSeries<DataPointInterface> mSeries2;
-    private boolean onpause=false;
+    private GraphView graph2;
+    private LineGraphSeries<DataPointInterface> mSeriesX, mSeriesY, mSeriesZ;
+    private Bitmap graph_view_PPG, graph_view_Acc;
+    private boolean onpause = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -139,13 +146,46 @@ public class ShimmerSpec extends AppCompatActivity {
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getViewport().setYAxisBoundsManual(true);
         graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(2000);
+        graph.getViewport().setMaxX(20);
         graph.getViewport().setMinY(1000);
         graph.getViewport().setMaxY(1900);
         graph.getViewport().setMaxYAxisSize(2000);
 
+        graph.setTitle("PPG Data");
+        graph.setTitleTextSize(55f);
 
 
+        graph2 = findViewById(R.id.graph2);
+
+        mSeriesX = new LineGraphSeries<>();
+        mSeriesZ = new LineGraphSeries<>();
+        mSeriesY = new LineGraphSeries<>();
+
+
+        mSeriesX.setColor(R.color.red);
+        mSeriesZ.setColor(R.color.blue);
+        mSeriesY.setColor(R.color.green);
+
+        graph2.addSeries(mSeriesX);
+        graph2.addSeries(mSeriesZ);
+        graph2.addSeries(mSeriesY);
+
+
+        graph2.getViewport().setScalable(true);
+        graph2.getViewport().setScalableY(false);
+        graph2.getViewport().setXAxisBoundsManual(true);
+        graph2.getViewport().setYAxisBoundsManual(true);
+        graph2.getViewport().setMinX(0);
+        graph2.getViewport().setMaxX(20);
+        graph2.getViewport().setMinY(-20);
+        graph2.getViewport().setMaxY(20);
+        graph2.getViewport().setMaxYAxisSize(20);
+
+        graph2.setTitle("Accelerometer Data");
+        graph2.setTitleTextSize(55f);
+
+        graph.setDrawingCacheEnabled(true);
+        graph2.setDrawingCacheEnabled(true);
 
         Slide slide = new Slide();
         slide.setSlideEdge(Gravity.BOTTOM);
@@ -171,6 +211,13 @@ public class ShimmerSpec extends AppCompatActivity {
 
         slide2.setInterpolator(new LinearOutSlowInInterpolator());
         getWindow().setExitTransition(slide2);
+
+// Request permissions
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.CAMERA,
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        }, 0);
 
         try {
             btManager = new ShimmerBluetoothManagerAndroid(this, mHandler);
@@ -218,7 +265,7 @@ public class ShimmerSpec extends AppCompatActivity {
         } else {
             if (shimmerDevice != null & !onpause) {
                 shimmerDevice.startStreaming();
-                onpause=true;
+                onpause = true;
             }
         }
     }
@@ -239,8 +286,7 @@ public class ShimmerSpec extends AppCompatActivity {
                 String macAdd = data.getStringExtra(EXTRA_DEVICE_ADDRESS);
 
                 nDialog = new ProgressDialog(ShimmerSpec.this);
-                nDialog.setMessage("Loading..");
-                nDialog.setTitle("Connecting");
+                nDialog.setMessage("Connecting..");
                 nDialog.setIndeterminate(false);
                 nDialog.setCancelable(true);
                 nDialog.show();
@@ -290,13 +336,58 @@ public class ShimmerSpec extends AppCompatActivity {
         super.onStop();
     }
 
+    public void operMenuSensor(View view) {
+
+
+        if (shimmerDevice != null) {
+            if (!shimmerDevice.isStreaming() && !shimmerDevice.isSDLogging()) {
+                //ShimmerDialogConfigurations.buildShimmerSensorEnableDetails(shimmerDevice, MainActivity.this);
+                ShimmerDialogConfigurations.buildShimmerSensorEnableDetails(shimmerDevice, ShimmerSpec.this, btManager);
+                Log.i("the end", "done");
+                graph.buildDrawingCache();
+                graph_view_PPG = Bitmap.createBitmap(graph.getDrawingCache());
+                graph.setDrawingCacheEnabled(false);
+                graph2.buildDrawingCache();
+                graph_view_Acc = Bitmap.createBitmap(graph2.getDrawingCache());
+                graph2.setDrawingCacheEnabled(false);
+                MediaStore.Images.Media.insertImage(getApplicationContext().getContentResolver(), graph_view_PPG, "PPG_Graph", "");
+                MediaStore.Images.Media.insertImage(getApplicationContext().getContentResolver(), graph_view_Acc, "ACC_Graph", "");
+            } else {
+                shimmerDevice.stopStreaming();
+                onpause = true;
+                ShimmerDialogConfigurations.buildShimmerSensorEnableDetails(shimmerDevice, ShimmerSpec.this, btManager);
+                graph.buildDrawingCache();
+                graph_view_PPG = Bitmap.createBitmap(graph.getDrawingCache());
+                graph.setDrawingCacheEnabled(false);
+                graph2.buildDrawingCache();
+                graph_view_Acc = Bitmap.createBitmap(graph2.getDrawingCache());
+                graph2.setDrawingCacheEnabled(false);
+                MediaStore.Images.Media.insertImage(getApplicationContext().getContentResolver(), graph_view_PPG, "PPG_Graph", "");
+                MediaStore.Images.Media.insertImage(getApplicationContext().getContentResolver(), graph_view_Acc, "ACC_Graph", "");
+                Log.i("the end", "done");
+
+            }
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle("Error")
+                    .setMessage("No shimmer device connected")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Log.d("MainActivity", "Done");
+                        }
+                    })
+                    .show();
+        }
+    }
+
     /**
      * Messages from the Shimmer device including sensor data are received here
      */
 
 
-   @SuppressLint("HandlerLeak")
-   Handler mHandler = new Handler() {
+    @SuppressLint("HandlerLeak")
+    Handler mHandler = new Handler() {
 
         @Override
         public void handleMessage(Message msg) {
@@ -313,19 +404,18 @@ public class ShimmerSpec extends AppCompatActivity {
                         double accel_x = 6;
                         double accel_z = 6;
                         double accel_y = 6;
-                        double timestamp=0;
-
+                        double timestamp = 0;
 
 
                         Collection<FormatCluster> allFormats = objectCluster.getCollectionOfFormatClusters(Configuration.Shimmer3.ObjectClusterSensorName.TIMESTAMP);
-                        FormatCluster formatCluster = ((FormatCluster)ObjectCluster.returnFormatCluster(allFormats,"CAL"));
+                        FormatCluster formatCluster = ((FormatCluster) ObjectCluster.returnFormatCluster(allFormats, "CAL"));
 //                        double timeStampData = formatCluster.mData;
 //                        Log.i(LOG_TAG, "Time Stamp: " + timeStampData);
                         timestamp = formatCluster.mData;
                         double dataPPG = 0;
                         allFormats = objectCluster.getCollectionOfFormatClusters("PPG_A13");
                         formatCluster = (ObjectCluster.returnFormatCluster(allFormats, "CAL"));
-                        if (formatCluster!=null) {
+                        if (formatCluster != null) {
                             dataPPG = formatCluster.mData;
                         }
 
@@ -346,22 +436,21 @@ public class ShimmerSpec extends AppCompatActivity {
                         }
 
                         double heartRate = Double.NaN;
-                        if (mCountPPGInitial < sampleRate*2){ //skip first 2 seconds
+                        if (mCountPPGInitial < sampleRate * 2) { //skip first 2 seconds
                             mCountPPGInitial++;
                         } else {
                             heartRate = mHeartRateCalculation.ppgToHrConversion(lpFilteredDataPPG, timestamp);
 
-                            if (heartRate==INVALID_OUTPUT) {
-                                heartRate=Double.NaN;
+                            if (heartRate == INVALID_OUTPUT) {
+                                heartRate = Double.NaN;
                             }
-
 
 
                         }
                         //objectCluster.mPropertyCluster.put("Heart Rate", new FormatCluster("CAL", "bpm", heartRate));
-                        Log.d("shimmerSensingHeartRate", "HR: "+heartRate);
+                        Log.d("shimmerSensingHeartRate", "HR: " + heartRate);
 
-                         if (formatCluster != null) {
+                        if (formatCluster != null) {
                             gsrConductance = formatCluster.mData;
                         }
                         allFormats = objectCluster.getCollectionOfFormatClusters(Configuration.Shimmer3.ObjectClusterSensorName.GSR_RESISTANCE);
@@ -399,18 +488,21 @@ public class ShimmerSpec extends AppCompatActivity {
                         }
 
 
-
                         Log.d(LOG_TAG, "DATA_PACKET: " +
                                 "\n GSR CONDUCTANCE: " + gsrConductance +
                                 "\n GSR RESISTANCE: " + gsrResistance +
                                 "\n PPG: " + ppg +
-                                "\n TEMPERATURE: " + temperature+
-                                "\n ACCELERATION_X: " + accel_x+
-                                "\n ACCELERATION_Z: " + accel_z+
+                                "\n TEMPERATURE: " + temperature +
+                                "\n ACCELERATION_X: " + accel_x +
+                                "\n ACCELERATION_Z: " + accel_z +
                                 "\n ACCELERATION_Y: " + accel_y);
 
 
-                        mSeries2.appendData(new DataPoint(timestamp, dataPPG), true, 500);
+                        mSeries2.appendData(new DataPoint(timestamp / 1000, dataPPG), true, 10000);
+
+
+                        double acceleration = Math.sqrt(Math.pow(accel_x, 2) + Math.pow(accel_z, 2) + Math.pow(accel_y, 2));
+                        mSeriesX.appendData(new DataPoint(timestamp / 1000, acceleration), true, 10000);
                     }
                     break;
                 case Shimmer.MESSAGE_TOAST:
@@ -436,19 +528,19 @@ public class ShimmerSpec extends AppCompatActivity {
                             shimmerDevice = btManager.getShimmerDeviceBtConnectedFromMac(shimmerBtAdd);
                             if (shimmerDevice != null) {
                                 Log.i(LOG_TAG, "Got the ShimmerDevice!");
-                                if(not_config_yet) {
+                                if (not_config_yet) {
                                     sampleRate = shimmerDevice.getSamplingRateShimmer();
                                     connected = true;
                                     mNewPPGSignalProcessing = true;
                                     mCountPPGInitial = 0;
                                     mHeartRateCalculation = new PPGtoHRAlgorithm(sampleRate, mNumberOfBeatsToAverage, 10); //10 second training period
-                                    not_config_yet=false;
+                                    not_config_yet = false;
 
-                                try {
-                                    mLPFilter = new Filter(Filter.LOW_PASS, sampleRate, mLPFc);
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
+                                    try {
+                                        mLPFilter = new Filter(Filter.LOW_PASS, sampleRate, mLPFc);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
 
                                 }
                                 nDialog.dismiss();
@@ -494,65 +586,6 @@ public class ShimmerSpec extends AppCompatActivity {
         }
     };
 
-//    public void enableHeartRate(String bluetoothAddress, boolean enabled, String sensorToHeartRate){
-//
-//
-//        mEnableHeartRate = enabled;
-//        if(enabled){
-//            mNewPPGSignalProcessing = true;
-//            mCountPPGInitial=0;
-//            mSensortoHR = sensorToHeartRate;
-//            mHeartRateCalculation = new PPGtoHRAlgorithm(sampleRate, mNumberOfBeatsToAverage,10); //10 second training period
-//
-//
-//            try {
-//                mLPFilter = new Filter(Filter.LOW_PASS, sampleRate, mLPFc);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-//            try {
-//                mHPFilter = new Filter(Filter.HIGH_PASS, sampleRate, mHPFc);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        else{
-//            mCountPPGInitial=0;
-//            mSensortoHR="";
-//        }
-//
-//
-//    }
-
-    public void operMenuSensor(View view) {
-
-
-        if (shimmerDevice != null) {
-            if (!shimmerDevice.isStreaming() && !shimmerDevice.isSDLogging()) {
-                //ShimmerDialogConfigurations.buildShimmerSensorEnableDetails(shimmerDevice, MainActivity.this);
-                ShimmerDialogConfigurations.buildShimmerSensorEnableDetails(shimmerDevice, ShimmerSpec.this, btManager);
-                Log.i("the end", "done");
-            } else {
-                shimmerDevice.stopStreaming();
-                onpause=true;
-                ShimmerDialogConfigurations.buildShimmerSensorEnableDetails(shimmerDevice, ShimmerSpec.this, btManager);
-                Log.i("the end", "done");
-
-            }
-        } else {
-            new AlertDialog.Builder(this)
-                    .setTitle("Error")
-                    .setMessage("No shimmer device connected")
-                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Log.d("MainActivity", "Done");
-                        }
-                    })
-                    .show();
-        }
-    }
 
     @Override
     protected void onDestroy() {
@@ -566,6 +599,8 @@ public class ShimmerSpec extends AppCompatActivity {
                 }
 
             }
+
+
         }
         super.onDestroy();
     }
